2d4d511580ff74ab19127a45cf2aad84254652b2
KamilBienias <49995107+KamilBienias@users.noreply.github.com>
2020-04-19 13:12:53 +0200

    Update ValidatorEnum.java
    corrected errMessage field

--- src/main/java/pl/java4me/hospitalspringbootjsp/validators/ValidatorEnum.java
+++ src/main/java/pl/java4me/hospitalspringbootjsp/validators/ValidatorEnum.java
@@ -2,11 +2,11 @@ package pl.java4me.hospitalspringbootjsp.validators;
 
 public enum ValidatorEnum {
 
-	FIRSTNAME("[a-zA-z]{3,}", "err_code", "User name should contain a minimum o 3 characters, which are letters or numbers"),
-	LASTNAME("[a-zA-z]{3,}", "err_code", "User name should contain a minimum o 3 characters, which are letters or numbers"),
+	FIRSTNAME("[a-zA-z]{3,}", "err_code", "User name should contain a minimum of 3 characters, which are letters or numbers"),
+	LASTNAME("[a-zA-z]{3,}", "err_code", "User name should contain a minimum of 3 characters, which are letters or numbers"),
 //	DATEOFBIRTH("[0-9]{2}-[0-9]{2}-[0-9]{4}","err_code", "Date of birth should look dd-MM-yyyy"),
 	PESEL("[0-9]{2}[0-3]{1}[0-9]{1}[0-3]{1}[0-9]{6}", "err_code", "The pesel should contain 11 digits, of which the first 6 form the birth date yy-MM-dd in database."),
-	PASSWORD("[a-zA-z0-9]{6,}", "err_code", "Password should contain a minimum o 6 characters, which are letters or numbers"),
+	PASSWORD("[a-zA-z0-9]{6,}", "err_code", "Password should contain a minimum of 6 characters, which are letters or numbers"),
 	NICKNAME("[a-z]{6}[0-9]{1}", "err_code", "Nickme should contain 6 characters and last digit"),
 //	EMAIL("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$","err_code", "Email format required");
 	SALARY("[0-9]{1,}", "err_code", "Salary shoud be a number.");
